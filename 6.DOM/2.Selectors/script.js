20732a38c08eb6063b81a402a2c3f2a4ab51a5a9
manu-cj <carenjotje@gmail.com>
2025-03-10 15:04:33 +0100

    2.Selectors done

--- 6.DOM/2.Selectors/script.js
+++ 6.DOM/2.Selectors/script.js
@@ -1,2 +1,30 @@
 /*
 */
+const important = document.querySelector('.important');
+important.title = 'This is an important item';
+
+const imgs = document.querySelectorAll('img');
+imgs.forEach(element => {
+    if (!element.classList.contains('important')) {
+        element.style.display = 'none';
+    }
+});
+
+const p = document.querySelectorAll('p');
+p.forEach(element => {
+    console.log(element.textContent);
+    if (element.classList.length > 0) {
+        console.log(element.classList);
+    } else {
+        element.style.color = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+
+    }
+}
+);
+
+function randomColor() {
+    return Math.floor(Math.random() * 256);
+}
+
+
+
