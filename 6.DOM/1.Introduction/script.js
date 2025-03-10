cc06599e0fbefbff6f911d5beab727b27a1254f2
manu-cj <carenjotje@gmail.com>
2025-03-10 14:48:45 +0100

    1.Introduction done

--- 6.DOM/1.Introduction/script.js
+++ 6.DOM/1.Introduction/script.js
@@ -1,2 +1,22 @@
 /*
 */
+
+const h1 = document.querySelector('h1');
+const body = document.querySelector('body');
+
+
+h1.innerText = 'Modifying the DOM!';
+console.log(h1);
+
+function randomColor() {
+    return Math.floor(Math.random() * 256);
+}
+
+body.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+
+const secondChild = body.children[1];
+
+for (const element of secondChild.children) {
+    console.log(element);
+}
+
