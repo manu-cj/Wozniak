5550070c600c32eda32cc5b48ecd0ac9dc9f5bd1
manu-cj <carenjotje@gmail.com>
2025-03-17 13:46:55 +0100

    5.Event done

--- 6.DOM/5.Events/script.js
+++ 6.DOM/5.Events/script.js
@@ -4,7 +4,19 @@ const getElapsedTime = () => {
   return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
 }
 
+const displayedsquareWrapper = document.querySelector('.displayedsquare-wrapper');
+const ul = document.querySelector('ul');
+let displayedsquare = document.querySelectorAll('.displayedsquare');
+
 const clickOnSquare = (e) => {
+  displayedsquareWrapper.appendChild(document.createElement('div'));
+
+  let newForm = displayedsquareWrapper.lastChild;
+  newForm.classList.add('displayedsquare', e.target.classList[1]);
+  ul.appendChild(document.createElement('li'));
+  let li = ul.lastChild;
+  li.innerText = getElapsedTime() + ' create a square ' + e.target.classList[1];
+  
   console.log(e.target.classList[1])
   console.log(getElapsedTime())
 }
@@ -14,3 +26,46 @@ for (let actionSquare of actionSquares) {
   actionSquare.addEventListener('click', clickOnSquare)
 }
 
+function randomColor() {
+  let blue = Math.floor(Math.random() * 256);
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+
+  let backgroundColor = 'rgb(' + red + ',' +green + ',' + blue + ')';
+  return backgroundColor;
+}
+
+document.addEventListener('keypress', (e) => {
+  if (e.code === "Space") {
+    let color = randomColor()
+    document.querySelector('body').style.backgroundColor = color;
+    ul.appendChild(document.createElement('li'));
+    let li = ul.lastChild;
+    li.innerText = getElapsedTime() + ' Background change for ' + color;
+  }
+
+  if (e.code === 'KeyI') {
+    let li = document.querySelectorAll('li');
+    li.forEach(element => {
+      element.remove();
+      
+    });
+  }
+
+  if (e.code === 'KeyS') {
+    displayedsquare.forEach(element => {
+      element.remove();
+    });
+  }
+})
+
+const clickOnSquare2 = (e) => {
+  console.log(e.target.classList[1])
+}
+
+
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('displayedsquare')) {
+    alert(e.target.classList[1]);
+  }
+});
