174ff578be908b03f55c4b0f3a5ea2d71a5b8e6f
manu-cj <carenjotje@gmail.com>
2025-03-18 09:51:11 +0100

    feat: 6.Forms done

---
+++ 6.DOM/6.Forms/script.js
@@ -0,0 +1,55 @@
+const firstName = document.querySelector('#firstname');
+const displayFirstname = document.querySelector('#display-firstname');
+
+firstName.addEventListener('keyup', () => {
+    displayFirstname.textContent = firstName.value;
+})
+
+const age = document.querySelector('#age');
+const aHardTruth = document.querySelector('#a-hard-truth');
+
+age.addEventListener('keyup', () => {
+    if (age.value < 18) {
+        aHardTruth.style.visibility = 'hidden'
+    } else {
+        aHardTruth.style.visibility = 'visible';
+    }
+});
+
+const pwd = document.querySelector('#pwd');
+const pwdConfirm = document.querySelector('#pwd-confirm');
+
+pwd.addEventListener('keyup', () => {
+    pwd.style.outline = 'none';
+    if (pwd.value.length < 6) {
+        pwd.style.border = '2px solid red';
+        pwd.setCustomValidity('Password must be at least 6 characters long');
+    } else {
+        pwd.style.border = '2px solid green'
+        pwd.setCustomValidity('');
+    }
+});
+
+pwdConfirm.addEventListener('keyup', () => {
+    pwdConfirm.style.outline = 'none';
+    if (pwdConfirm.value !== pwd.value) {
+        pwdConfirm.style.border = '2px solid red';
+        pwdConfirm.setCustomValidity('Passwords do not match');
+    } else {
+        pwdConfirm.style.border = '2px solid green';
+        pwdConfirm.setCustomValidity('');
+    }
+});
+
+const toggleDarkmode = document.querySelector('#toggle-darkmode');
+
+toggleDarkmode.addEventListener('change', () => {
+    if (toggleDarkmode.value === 'dark') {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+    }
+    else if(toggleDarkmode.value === 'light') {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+    }
+});
